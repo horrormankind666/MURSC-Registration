b4d7b7598c76460e1e9b5cfcbceef441b288e78a
User <User@SAYONGHOUSESERV>
2020-08-05 15:06:35 +0700

    '...'

--- src/app/transaction/registered/home/transaction-registered-home.component.ts
+++ src/app/transaction/registered/home/transaction-registered-home.component.ts
@@ -2,7 +2,7 @@
 =============================================
 Author      : <ยุทธภูมิ ตวันนา>
 Create date : <๐๘/๐๖/๒๕๖๓>
-Modify date : <๒๙/๐๗/๒๕๖๓>
+Modify date : <๐๕/๐๘/๒๕๖๓>
 Description : <>
 =============================================
 */
@@ -11,12 +11,18 @@ Description : <>
 
 import {Component, OnInit} from '@angular/core';
 
+import {AppService} from '../../../app.service';
+import {DataService} from '../../../data.service';
+
 @Component({
   selector: 'app-transaction-registered-home',
-  templateUrl: './transaction-registered-home.component.html'
+  templateUrl: './transaction-registered-home.component.html',
+  styleUrls: ['./transaction-registered-home.component.scss']
 })
 export class TransactionRegisteredHomeComponent implements OnInit {
   constructor(
+    private appService: AppService,
+    private dataService: DataService
   ) {}
 
   ngOnInit() {
